2bd2f8973c9d63be448d317f4e0ccef7d3ce3efa
ongchangco <kongchangcok.gmail.com>
2024-11-21 09:40:36 +0800

    Added Button Functionalities MainActivity: btnLogIn Dashboard: btnFaM, btnMNav, btnQRScanner, btnMYAMUSE

--- app/src/main/java/smart/museum/amuse/Dashboard.kt
+++ app/src/main/java/smart/museum/amuse/Dashboard.kt
@@ -1,6 +1,8 @@
 package smart.museum.amuse
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,31 @@ class Dashboard : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnFaM = findViewById<Button>(R.id.btnFaM)
+        val btnMNav = findViewById<Button>(R.id.btnMNav)
+        val btnQrScanner = findViewById<Button>(R.id.btnQrScanner)
+        val btnMYAMUSE = findViewById<Button>(R.id.btnMYAMUSE)
+
+        btnFaM.setOnClickListener{
+            val goto_MFinder = Intent(this, MuseumFinder::class.java)
+            startActivity(goto_MFinder)
+        }
+
+        btnMNav.setOnClickListener{
+            val goto_MNav = Intent(this, MuseumNavigator::class.java)
+            startActivity(goto_MNav)
+        }
+
+        btnQrScanner.setOnClickListener{
+            val goto_QR = Intent(this, QrScanner::class.java)
+            startActivity(goto_QR)
+        }
+
+        btnMYAMUSE.setOnClickListener{
+            val goto_MYAMUSE = Intent(this, MyAMUSE::class.java)
+            startActivity(goto_MYAMUSE)
         }
     }
 }
--- app/src/main/java/smart/museum/amuse/MainActivity.kt
+++ app/src/main/java/smart/museum/amuse/MainActivity.kt
@@ -1,6 +1,9 @@
 package smart.museum.amuse
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,11 +13,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_qr_code_result)
+        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+        }
+
+        val btnLogIn = findViewById<Button>(R.id.btnLogin)
+
+        btnLogIn.setOnClickListener{
+            val goto_Dashboard = Intent(this, Dashboard::class.java)
+            startActivity(goto_Dashboard)
         }
     }
 }

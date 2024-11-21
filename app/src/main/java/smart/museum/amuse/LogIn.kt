9d1b9d245685647058ef395ce6525af111f2d9f3
Franchesca-Urbi <ckeskaa@gmail.com>
2024-11-21 21:47:48 +0800

    Added Login Functionalities - Username & Password Input Log - Login Button

--- app/src/main/java/smart/museum/amuse/LogIn.kt
+++ app/src/main/java/smart/museum/amuse/LogIn.kt
@@ -1,12 +1,21 @@
 package smart.museum.amuse
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class LogIn : AppCompatActivity() {
+
+    lateinit var uInput : EditText
+    lateinit var pInput : EditText
+    lateinit var loginBtn : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +24,21 @@ class LogIn : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        uInput = findViewById(R.id.userInput)
+        pInput = findViewById(R.id.passInput)
+
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+
+        btnLogin.setOnClickListener{
+            val goto_Login = Intent(this, Dashboard::class.java)
+            startActivity(goto_Login)
+
+            val username = uInput.text.toString()
+            val password = pInput.text.toString()
+            Log.i("Credentials", "Username : $username and Password : $password")
+
         }
     }
 }

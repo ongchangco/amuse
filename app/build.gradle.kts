a3f40f239e40712d36e3a1b1a8f9a901c8cb0644
Franchesca-Urbi <ckeskaa@gmail.com>
2024-11-30 01:34:39 +0800

    Added API Key

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,4 +50,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.google.android.gms:play-services-maps:19.0.0")
+    implementation("com.google.android.gms:play-services-ads:23.5.0")
 }

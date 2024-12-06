edf0a3e163567d1f5de005bd7f2422e28a0ac005
Franchesca-Urbi <ckeskaa@gmail.com>
2024-12-07 01:34:21 +0800

    Removed unused variables & unnecessary imports

--- app/src/main/java/smart/museum/amuse/LogIn.kt
+++ app/src/main/java/smart/museum/amuse/LogIn.kt
@@ -2,19 +2,13 @@ package smart.museum.amuse
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
-import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class LogIn : AppCompatActivity() {
-
-    lateinit var uInput : EditText
-    lateinit var pInput : EditText
-    lateinit var loginBtn : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
